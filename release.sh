04de0ec9eb51c775c188879792de10862ed57d35
develar <develar@gmail.com>
2017-12-15 10:33:53 +0100

    add release.sh

---
+++ release.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ -z "$GITHUB_TOKEN" ] ; then
+  SEC=`security find-generic-password -l GH_TOKEN -g 2>&1`
+  export GITHUB_TOKEN=`echo "$SEC" | grep "password" | cut -d \" -f 2`
+fi
+
+goreleaser --rm-dist "$@"
